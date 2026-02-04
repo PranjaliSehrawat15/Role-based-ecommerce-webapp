7a1d3b97859dd98eb7de88f60c04b46592625a4c
Pranjali Sehrawat <pranjalisehrawat@gmail.com>
2026-02-04 11:32:27 +0530

    some changes

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,22 +1,19 @@
 import { initializeApp } from "firebase/app"
 import { getAuth } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
-//import { getStorage } from "firebase/storage"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCPqWDEGjFzMTQZdgAS1lQnAFFSp30n7_4",
-  authDomain: "role-based-ecom.firebaseapp.com",
-  projectId: "role-based-ecom",
-  storageBucket: "role-based-ecom.firebasestorage.app",
-  messagingSenderId: "Y466489047562",
-  appId: "1:466489047562:web:531be6a86fce537f853df3",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 }
 
 const app = initializeApp(firebaseConfig)
 
 export const auth = getAuth(app)
 export const db = getFirestore(app)
-//export const storage = getStorage(app)
-
 
 
